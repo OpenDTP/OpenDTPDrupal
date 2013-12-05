e907d8bcee4d81e133df41ef08450c0bfcedd784
exadeci <exadeci@gmail.com>
2013-12-05 13:05:56 +0100

    Slide Implementation start

---
+++ sites/all/modules/opendtp/theme/opendtp.tpl.php
@@ -0,0 +1,6 @@
+				<link rel="stylesheet" href="imageflow.packed.css" type="text/css" />
+		<script type="text/javascript" src="imageflow.packed.js"></script>
+		<div id="myImageFlow" class="imageflow">
+			<img src="img/img1.png" longdesc="img/img1.png" width="400" height="300" alt="Image 1" />
+			$values
+		</div>
