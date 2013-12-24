e6c0bae661300ff0a2a23c2cdaeef04e9bfa70aa
Gaetan Gueraud <gaetan.gueraud@gmail.com>
2013-12-24 17:24:35 +0100

    Testing ImageFlow

--- sites/all/modules/opendtp/opendtp.tpl.php
+++ sites/all/modules/opendtp/opendtp.tpl.php
@@ -1,4 +1,26 @@
-TEST
-<?php
-echo $content
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>ImageFlow</title>
+		<meta name="robots" content="index, follow, noarchive" />
+		<link rel="stylesheet" href="style.css" type="text/css" />
+
+		<!-- This includes the ImageFlow CSS and JavaScript -->
+		<link rel="stylesheet" href="imageflow.packed.css" type="text/css" />
+		<script type="text/javascript" src="imageflow.packed.js"></script>
+
+	</head>
+	<body>
+		<h1>ImageFlow</h1>
+
+		<!-- This is all the XHTML ImageFlow needs -->
+		<div id="myImageFlow" class="imageflow">
+			<img src="img/img1.png" longdesc="img/img1.png" width="400" height="300" alt="Image 1" />
+			<?php
+			echo $content
+			?>
+		</div>
+
+	</body>
+</html>
