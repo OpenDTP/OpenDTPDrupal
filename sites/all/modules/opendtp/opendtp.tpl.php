055ec3b92e3154255e09b8a118ccdc71e0fc7187
Gaetan Gueraud <gaetan.gueraud@gmail.com>
2013-12-24 19:38:26 +0100

    Testing ImageFlow

--- sites/all/modules/opendtp/opendtp.tpl.php
+++ sites/all/modules/opendtp/opendtp.tpl.php
@@ -4,11 +4,11 @@
 		<meta charset="utf-8" />
 		<title>ImageFlow</title>
 		<meta name="robots" content="index, follow, noarchive" />
-		<link rel="stylesheet" href=<?php echo $module_path; ?>"/style.css" type="text/css" />
+		<link rel="stylesheet" href=<?php echo $module_path; ?>/style.css" type="text/css" />
 
 		<!-- This includes the ImageFlow CSS and JavaScript -->
-		<link rel="stylesheet" href=<?php echo $module_path; ?>"/imageflow.packed.css" type="text/css" />
-		<script type="text/javascript" src=<?php echo $module_path; ?>"/imageflow.packed.js"></script>
+		<link rel="stylesheet" href=<?php echo $module_path; ?>/imageflow.packed.css" type="text/css" />
+		<script type="text/javascript" src=<?php echo $module_path; ?>/imageflow.packed.js"></script>
 
 	</head>
 	<body>
@@ -16,7 +16,7 @@
 
 		<!-- This is all the XHTML ImageFlow needs -->
 		<div id="myImageFlow" class="imageflow">
-			<img src=<?php echo $module_path; ?>"/img/img1.png" longdesc=<?php echo $module_path; ?>"/img/img1.png" width="400" height="300" alt="Image 1" />
+			<img src=<?php echo $module_path; ?>/img/img1.png" longdesc=<?php echo $module_path; ?>/img/img1.png" width="400" height="300" alt="Image 1" />
 			
 		</div>
 
