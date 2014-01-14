0e195d35f67f59b9c2cde9d4bc653ed31fe00f53
exadeci <exadeci@gmail.com>
2014-01-14 17:23:19 +0100

    Jstree template refactor

--- sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
+++ sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
@@ -1,39 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8" />
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>jsTree test</title>
+  <!-- 2 load the theme CSS file -->
 		<link rel="stylesheet" href=<?php echo $module_path; ?>/jstree/dist/themes/default/style.min.css />
-	</head>
-	<body>
-		<div id="jstree"><div>
-			<ul>
-			  <li>Root
-				<ul>
-				  <li id="child_node_1">Child node 1</li>
-				  <li>Child node 2</li>
-				</ul>
-			  </li>
-			  <li>Root node 2</li>
-			</ul>
-		  </div>
-		  <button>This amazing button</button>
-		<script src=<?php echo $module_path; ?>/jstree/dist/libs/jquery.js></script>
-		<script src=<?php echo $module_path; ?>/jstree/dist/jstree.min.js></script>
-		<script>
-		  $(function () {
-			// 6 create an instance when the DOM is ready
-			$('#jstree').jstree();
-			// 7 bind to events triggered on the tree
-			$('#jstree').on("changed.jstree", function (e, data) {
-			  console.log(data.selected);
-			});
-			// 8 interact with the tree - either way is OK
-			$('button').on('click', function () {
-			  $('#jstree').jstree(true).select_node('child_node_1');
-			  $('#jstree').jstree('select_node', 'child_node_1');
-			  $.jstree.reference('#jstree').select_node('child_node_1');
-			});
-		  });
-		</script>
-	</body>
+</head>
+<body>
+  <!-- 3 setup a container element -->
+  <div id="jstree">
+    <!-- in this example the tree is populated from inline HTML -->
+    <ul>
+      <li>Root node 1
+        <ul>
+          <li id="child_node_1">Child node 1</li>
+          <li>Child node 2</li>
+        </ul>
+      </li>
+      <li>Root node 2</li>
+    </ul>
+  </div>
+  <button>demo button</button>
+
+  <script src=<?php echo $module_path; ?>/jstree/dist/libs/jquery.js></script>
+  <script src=<?php echo $module_path; ?>/jstree/dist/jstree.min.js></script>
+  <script>
+  $(function () {
+    // 6 create an instance when the DOM is ready
+    $('#jstree').jstree();
+    // 7 bind to events triggered on the tree
+    $('#jstree').on("changed.jstree", function (e, data) {
+      console.log(data.selected);
+    });
+    // 8 interact with the tree - either way is OK
+    $('button').on('click', function () {
+      $('#jstree').jstree(true).select_node('child_node_1');
+      $('#jstree').jstree('select_node', 'child_node_1');
+      $.jstree.reference('#jstree').select_node('child_node_1');
+    });
+  });
+  </script>
+</body>
 </html>
