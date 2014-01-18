b58e0e22fc60f0e84c5aa8dc82cf67f9b089bf8f
Gaetan Gueraud <exadeci@gmail.com>
2014-01-18 12:08:46 +0100

    Type check

--- sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
+++ sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
@@ -8,35 +8,37 @@
 <body>
   <div id="jstree">
     <ul>
-      <li>Root node 1
+      <li>Root
         <ul>
-          <li id="child_node_1">Child node 1</li>
-          <li>Child node 2</li>
+          <li>Article</li>
+          <ul>
+
+          </ul>
+          <li>Page</li>
+          <ul>
+          
+          </ul>
+          <li>Master</li>
+          <ul>
+          
+          </ul>
         </ul>
       </li>
-      <li>Root node 2</li>
     </ul>
   </div>
-  <button>demo button</button>
   <?php
     foreach ($query as $key) 
-      echo $key
+      echo $key['type'];
   ?>
   <script src=<?php echo $module_path; ?>/jstree/dist/libs/jquery.js></script>
   <script src=<?php echo $module_path; ?>/jstree/dist/jstree.min.js></script>
   <script>
-  $(function () {
-    // 6 create an instance when the DOM is ready
+  $(function ()
+  {
     $('#jstree').jstree();
-    // 7 bind to events triggered on the tree
-    $('#jstree').on("changed.jstree", function (e, data) {
+    $('#jstree').on("changed.jstree", function (e, data)
+    {
       console.log(data.selected);
-    });
-    // 8 interact with the tree - either way is OK
-    $('button').on('click', function () {
-      $('#jstree').jstree(true).select_node('child_node_1');
-      $('#jstree').jstree('select_node', 'child_node_1');
-      $.jstree.reference('#jstree').select_node('child_node_1');
     });
   });
   </script>
