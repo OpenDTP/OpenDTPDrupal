9422d61530ab775f7f7f9c6820203bedb5d21eee
Gaetan Gueraud <exadeci@gmail.com>
2014-01-18 14:51:28 +0100

    df

--- sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
+++ sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
@@ -10,27 +10,30 @@
     <ul>
       <li>Root
         <ul>
-          <li>Article</li>
-          <ul>
-            <?php
-              if ($query->type === "article")
-                echo "<li>" + $query->title + "</li>";
-             ?>
-          </ul>
-          <li>Page</li>
-          <ul>
-            <?php
-              if ($query->type === "page")
-                echo "<li>" + $query->title + "</li>";
-             ?>
-          </ul>
-          <li>Master</li>
-          <ul>
-            <?php
-              if ($query->type === "master")
-                echo "<li>" + $query->title + "</li>";
-             ?>
-          </ul>
+          <li>Article
+            <ul>
+              <?php
+                if ($query->type === "article")
+                 echo "<li>" + $query->title + "</li>";
+              ?>
+            </ul>
+          </li>
+          <li>Page
+            <ul>
+              <?php
+                if ($query->type === "page")
+                  echo "<li>" + $query->title + "</li>";
+               ?>
+            </ul>
+          </li>
+          <li>Master
+            <ul>
+              <?php
+                if ($query->type === "master")
+                  echo "<li>" + $query->title + "</li>";
+               ?>
+            </ul>
+          </li>
         </ul>
       </li>
     </ul>
@@ -38,7 +41,7 @@
   <script src=<?php echo $module_path; ?>/jstree/dist/libs/jquery.js></script>
   <script src=<?php echo $module_path; ?>/jstree/dist/jstree.min.js></script>
   <script>
-  $(function onAfterLoad()
+  $(function ()
   {
     $('#jstree').jstree();
     $('#jstree').on("changed.jstree", function (e, data)
