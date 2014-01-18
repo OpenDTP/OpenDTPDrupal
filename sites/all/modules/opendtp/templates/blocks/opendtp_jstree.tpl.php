7688627fa20b414ffe2c1448d31971210921c9ac
Gaetan Gueraud <exadeci@gmail.com>
2014-01-18 14:15:18 +0100

    h

--- sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
+++ sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
@@ -36,7 +36,7 @@
     </ul>
   </div>
               <?php
-                echo query->type;
+                echo $query->type;
              ?>
   <script src=<?php echo $module_path; ?>/jstree/dist/libs/jquery.js></script>
   <script src=<?php echo $module_path; ?>/jstree/dist/jstree.min.js></script>
