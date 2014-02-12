5e8a474c5571a02154df9eed2ef36e6d89cc7c70
exadeci <exadeci@gmail.com>
2014-02-12 14:14:12 +0100

    Addition of icons for Article and Page

--- sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
+++ sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
@@ -10,7 +10,7 @@
     <ul>
       <li>Root
         <ul>
-          <li>Article
+          <li data-jstree='{"icon":"<?php echo $module_path; ?>/assets/icons/article.png"}>Article
             <ul>
               <?php 
                 foreach ($query['article'] as $key)
@@ -18,7 +18,7 @@
               ?>
             </ul>
           </li>
-          <li>Page
+          <li data-jstree='{"icon":"<?php echo $module_path; ?>/assets/icons/page.png"}>Page
             <ul>
               <?php 
                 foreach ($query['page'] as $key)
