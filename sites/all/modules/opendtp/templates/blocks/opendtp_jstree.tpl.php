3e18eb1d5bd92c74eadd25057eb0411574030f42
exadeci <exadeci@gmail.com>
2014-02-12 14:22:01 +0100

    Addition of root icons/root.png

--- sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
+++ sites/all/modules/opendtp/templates/blocks/opendtp_jstree.tpl.php
@@ -2,15 +2,14 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>jsTree test</title>
 		<link rel="stylesheet" href=<?php echo $module_path; ?>/jstree/dist/themes/default/style.min.css />
 </head>
 <body>
   <div id="jstree">
     <ul>
-      <li>Root
+      <li data-jstree='{"opened":true,"selected":true,"icon":<?php echo $module_path; ?>/assets/icons/root.png}>Root
         <ul>
-          <li data-jstree='{"icon":"<?php echo $module_path; ?>/assets/icons/article.png"}>Article
+          <li data-jstree='{"icon":<?php echo $module_path; ?>/assets/icons/article.png}>Article
             <ul>
               <?php 
                 foreach ($query['article'] as $key)
@@ -18,7 +17,7 @@
               ?>
             </ul>
           </li>
-          <li data-jstree='{"icon":"<?php echo $module_path; ?>/assets/icons/page.png"}>Page
+          <li data-jstree='{"icon":<?php echo $module_path; ?>/assets/icons/page.png}>Page
             <ul>
               <?php 
                 foreach ($query['page'] as $key)
